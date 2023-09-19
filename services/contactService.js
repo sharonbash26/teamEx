22d74d323ecc2c9e2071e46711e876ff2c6a87d3
sharonbash26 <sharon.bash@live.biu.ac.il>
2023-09-19 16:34:03 +0300

    check

--- services/contactService.js
+++ services/contactService.js
@@ -12,7 +12,7 @@ export const contactsService = {
     remove,
     save,
 }
-
+console.log('hi')
 function query() {
     return asyncStorageService.query(STORAGE_KEY).then(contacts => {
         console.log('contacts', contacts)
