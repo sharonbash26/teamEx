4b9eda3003f2139ffe87c4edee860a8373d70680
sharonbash26 <sharon.bash@live.biu.ac.il>
2023-09-19 18:27:59 +0300

    F/add-router-add test

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -6,12 +6,13 @@ import { About } from "./views/About.jsx"
 import { Home } from "./views/Home.jsx"
 import { ContactIndex } from "./pages/ContactIndex.jsx"
 
-
+console.log('hiiiiiii')
 
 export function App() {
     return <Router>
         <section className="app">
             <AppHeader />
+            
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
---
+++ pages/ContactIndex.jsx
@@ -0,0 +1,17 @@
+const { useState, useEffect } = React
+const { useSelector, useDispatch } = ReactRedux
+import { contactsService } from "../services/contactService.js"
+export function ContactIndex(){
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        contactsService.query().then(contacts=>{
+            console.log('contacts',contacts)
+        })
+    },[])
+return(
+   <section className="contact-container">
+    <h1>contact container</h1>
+    <h1>contact container</h1>
+   </section> 
+)
+}
