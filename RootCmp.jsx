2cb4f6ae1bc42ff8b5bed862e24599185bbb0b6c
sharonbash26 <sharon.bash@live.biu.ac.il>
2023-09-19 16:30:48 +0300

    first commit

---
+++ RootCmp.jsx
@@ -0,0 +1,20 @@
+const { Route, Routes } = ReactRouterDOM
+const Router = ReactRouterDOM.HashRouter
+
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { About } from "./views/About.jsx"
+import { Home } from "./views/Home.jsx"
+
+
+
+export function App() {
+    return <Router>
+        <section className="app">
+            <AppHeader />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </section>
+    </Router>
+}
---
+++ services/contactService.js
@@ -0,0 +1,69 @@
+import { utilService } from "./util.service.js"
+import { asyncStorageService } from "./async-storage.service.js"
+import { storageService } from "./storage.service.js"
+
+const STORAGE_KEY = 'contactDB'
+var contacts = _createContacts()
+
+
+export const contactsService = {
+    query,
+    get,
+    remove,
+    save,
+}
+
+function query() {
+    return asyncStorageService.query(STORAGE_KEY).then(contacts => {
+        console.log('contacts', contacts)
+        return contacts
+    })
+}
+
+function get(contactId) {
+    return asyncStorageService.get(STORAGE_KEY, contactId)
+}
+
+
+
+
+function remove(contactId) {
+    return asyncStorageService.remove(STORAGE_KEY, contactId)
+}
+
+function save(contact) {
+    console.log('contact', contact)
+    if (contact.id) {
+        return asyncStorageService.put(STORAGE_KEY, contact)
+    } else {
+        return asyncStorageService.post(STORAGE_KEY, contact)
+    }
+}
+
+
+function _createContacts() {
+    console.log('start create contacts')
+    let contacts = storageService.loadFromStorage(STORAGE_KEY)
+    if (!contacts || !contacts.length) {
+        contacts = [{
+            id: utilService.makeId(),
+            firstName: 'sharon',
+            lastName:'bash',
+            phone:'0528979145',
+            desc:'hhhh',
+            mail: 'sharon26mo@gmail.com'
+        },
+        {
+            id: utilService.makeId(),
+            firstName: 'avishai',
+            lastName:'cohen',
+            mail:'avishaic2016@gmail.com',
+            desc:'ggg',
+            phone:'052897978'
+        }
+        ]
+    }
+    utilService.saveToStorage(STORAGE_KEY, contacts)
+    console.log('all todos', contacts)
+    return contacts
+}
